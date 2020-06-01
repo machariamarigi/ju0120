91141c3c638eb9a23fe7105ced109dfd83064238
machariamarigi <machariamarigi@gmail.com>
2020-06-01 21:27:58 +0300

    set up frontend

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import Bundler from 'parcel-bundler';
+import path from 'path';
 
 const app = express();
 const port = 8080 || process.env.PORT;
+const bundler = new Bundler(path.join(__dirname, '../src/client/index.html'));
+
+app.use(bundler.middleware());
 
 app.get("/", (req, res) => {
   res.send("Hi")
