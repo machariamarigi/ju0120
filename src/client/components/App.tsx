d5b2aa11c80b6c2edb96c0630cc318ee050012ea
machariamarigi <machariamarigi@gmail.com>
2020-06-02 17:53:14 +0300

    implement NewMessage and MessageList components

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,3 +1,14 @@
-import React from "react";
+import React from 'react';
+import io from 'socket.io-client';
 
-export default () => <div>Hello world</div>;
+import MessageList from './MessageList';
+import NewMessage from './NewMessage'
+
+const socket: any = io(location.origin)
+
+export default () => (
+  <div>
+    <MessageList socket={socket} />
+    <NewMessage socket={socket} />
+  </div>
+);
