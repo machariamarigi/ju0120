b0ea1f64efc8d284e9874021b8c45281f9e288a5
machariamarigi <machariamarigi@gmail.com>
2020-06-02 13:47:46 +0300

    set up socketio

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import express from 'express';
 import Bundler from 'parcel-bundler';
 import path from 'path';
+import http from 'http';
+import socketIOServer from 'socket.io';
+
+import initializeSocketIO from './socket';
 
 const app = express();
+const server = new http.Server(app);
+const io = socketIOServer(server);
 const port = 8080 || process.env.PORT;
 const bundler = new Bundler(path.join(__dirname, '../src/client/index.html'));
 
+initializeSocketIO(io);
 app.use(bundler.middleware());
 
-app.get("/", (req, res) => {
-  res.send("Hi")
-})
-
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`)
 })
